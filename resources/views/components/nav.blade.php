44395275a3cc2de518d78479db680ba94fb40bfe
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-12-02 15:52:43 +0100

    nav bar demo 1

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,18 @@
+<nav>
+    <img id="nav-OH-logo" src="{{ Vite::asset('resources/images/Logo.png') }}">
+    <div class="nav-center">
+        <a class="nav-item" href="/">Home</a>
+        <div class="dropdown">
+            <span class="nav-item">About</span>
+            <div class="dropdown-menu">
+                <a href="/about-us">About Us</a>
+                <a href="/open-hiring">How does Open Hiring work?</a>
+            </div>
+        </div>
+        <a class="nav-item" href="/contact">Contact</a>
+        <a class="nav-item" href="/vacatures">Vacatures</a>
+    </div>
+    <div class="nav-right">
+        <a class="nav-item" href="/aanmelden">Login/Registreer</a>
+    </div>
+</nav>
