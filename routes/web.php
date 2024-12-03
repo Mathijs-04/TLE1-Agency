eff7403418ed4806ca3432ff0dba433e332f75cf
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-12-03 10:54:29 +0100

    Created invite blade

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/uitnodigen', function () {
+    return view('invite');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
