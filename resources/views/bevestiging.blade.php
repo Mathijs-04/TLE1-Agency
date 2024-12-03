a3ff80608a652b0e2bafdbf91bd2399c787f44c2
Dirk van Gelderen <94358540+ItsMySquid@users.noreply.github.com>
2024-12-03 10:59:59 +0100

    Bevestigings pagina na het uitnodigen

---
+++ resources/views/bevestiging.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Bevestiging</title>
+</head>
+<body>
+<div class="flex items-center justify-center h-screen">
+    <div class="w-full md:w-full">
+        <p class="text-center text-5xl font-bold text-gray-700 mb-6">Morgen werk voor jou!</p>
+        <p class="text-center leading-relaxed text-gray-800 mb-6">
+            Met Open Hiring heeft iedereen een eerlijke kans<br/>op een baan. Wie wil én kan werken, kan zó aan de
+            slag.
+            <br/>Zonder sollicitatiegesprek, zonder brief, zonder vragen. <br/>Met één druk op de knop. Open Hiring
+            draait namelijk <br/>niet om diploma’s, maar om mensen. Niet om praatjes, <br/>maar om aanpakken.
+        </p>
+        <div class="flex items-center justify-center">
+            <button
+                class="relative bg-violetOH-500 hover:bg-violetOH-600 w-72 h-14 rounded-full border-b-4 border-[#7c1a51] font-bold text-cream text-base">
+                Bekijk vacature
+            </button>
+        </div>
+    </div>
+</div>
+</body>
+</html>
