c3deaa3f0e5e6bc4f49f9a3bd5f7bb8dd70e742e
StefanMuijs <145674829+StefanMuijs@users.noreply.github.com>
2024-12-03 09:20:12 +0100

    Added vacansy controller and view

---
+++ resources/views/my-vacancies.blade.php
@@ -0,0 +1,10 @@
+@vite('/resources/css/app.css')
+
+
+<div class="myvacansy1">
+
+    <img src="{{ Vite::asset('resources/images/Logo-De-Beren.jpg') }}" alt="Logo" />
+    <h1>De beren</h1>
+    <h2>Wachtende:2</h2>
+    <button>Uitnodigen</button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\vacansyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('my-vacancies', VacansyController::class);
