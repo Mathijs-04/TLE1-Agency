219957b440ad46d2da378d6d3e54a6a5f429efe3
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-12-03 11:52:32 +0100

    layout met navbar

---
+++ resources/views/components/Layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Document')</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+<x-nav> </x-nav>
+
+{{$slot}}
+
+</body>
+</html>
--- resources/views/vacatures.blade.php
+++ resources/views/vacatures.blade.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Vacatures</title>
-
-</head>
-<body>
-<x-nav> </x-nav>
-vacatures
-</body>
-</html>
+<x-Layout>
+    @section('title', 'vacature')
+    vaceture
+</x-Layout>
