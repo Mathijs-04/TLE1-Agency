e8496ef97adc31b4e4d0d75ec43a00fe61fb3f92
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-12-04 13:25:21 +0100

    Edited button text

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -4,7 +4,7 @@
             <h1 class="text-5xl font-bold text-gray-700 mb-6">404</h1>
             <h3 class="text-2xl text-gray-800 mb-6">This page does not yet exist.</h3>
             <a href="{{ url('/') }}" class="relative bg-violetOH-500 hover:bg-violetOH-600 w-72 h-14 rounded-full border-b-4 border-[#7c1a51] font-bold text-cream text-base flex items-center justify-center ml-5">
-                Return Home
+                Terug naar Home
             </a>
         </div>
     </div>
