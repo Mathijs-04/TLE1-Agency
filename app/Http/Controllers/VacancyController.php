656de57235627036d6c291897fd184fc881ebf4e
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-12-09 15:07:04 +0100

    Edited post-upload preview

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -31,9 +31,6 @@ class VacancyController extends Controller
      */
     public function store(Request $request)
     {
-        //
-
-
         $request->validate([
             'name' => 'required|string|max:255',
             'salary' => 'required|string|max:255',
@@ -43,24 +40,18 @@ class VacancyController extends Controller
             'description' => 'nullable|string',
             'requirement' => 'nullable|string',
             'image_url' => 'nullable|image|max:2048',
-            'waiting' => 'nullable|integer|min:0', // Optioneel veld
-            'available_positions' => 'nullable|integer|min:0', // Optioneel veld
-            'employer_id' => 'nullable|integer|min:0', // Laat employer_id optioneel
+            'waiting' => 'nullable|integer|min:0',
+            'available_positions' => 'nullable|integer|min:0',
+            'employer_id' => 'nullable|integer|min:0',
         ]);
-
 
         $vacancy = new Vacancy();
         $employerId = auth()->user()->employer_id;
 
-//        insert into "vacancies" ("name", "salary", "location", "hours", "contract_type", "description", "requirement", "waiting", "available_positions", "employer_id", "updated_at", "created_at")
-//        values (Dawn Peck, Sed ipsum sequi sed, Sint deleniti fugit, 17, full-time, Porro voluptatum fug, ?, 1, 1, 1, 2024-12-04 12:44:21, 2024-12-04 12:44:21)
-
-        // Afbeelding uploaden
         if ($request->hasFile('image_url')) {
-            $nameOfFile = $request->file('image_url')->store('images', 'public'); // Geen slash aan het begin
+            $nameOfFile = $request->file('image_url')->store('images', 'public');
             $vacancy->image_url = $nameOfFile;
         }
-
 
         $vacancy->name = $request->input('name');
         $vacancy->salary = $request->input('salary');
@@ -69,18 +60,13 @@ class VacancyController extends Controller
         $vacancy->contract_type = $request->input('contract_type');
         $vacancy->description = $request->input('description');
         $vacancy->requirement = $request->input('requirement');
+        $vacancy->waiting = $request->input('waiting', 3);
+        $vacancy->available_positions = $request->input('available_positions', 1);
+        $vacancy->employer_id = $employerId;
 
-// Check of de velden zijn ingevuld of stel een standaardwaarde in
-        $vacancy->waiting = $request->input('waiting', 3); // Standaardwaarde 3
-        $vacancy->available_positions = $request->input('available_positions', 1); // Standaardwaarde null
-        $vacancy->employer_id = $employerId; // Zorg ervoor dat ID 1 bestaat in de employers-tabel
-
-//        dd($vacancy);
         $vacancy->save();
 
-
-        return redirect()->route('mijn-vacatures.index');
-
+        return view('detail-vacancies', compact('vacancy'));
     }
 
     /**
