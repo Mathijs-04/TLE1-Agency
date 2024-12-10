f90b82adf0adecd81ecca1683dc06b6b4524ea68
Chris Tang <1073412@hr.nl>
2024-12-10 09:51:28 +0100

    UPDATE VacancyController.php

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -29,10 +29,135 @@ class VacancyController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+//    public function store(Request $request)
+//    {
+//        //
+//
+//        $request->validate([
+//            'name' => 'required|string|max:255',
+//            'salary' => 'required|numeric|min:0',
+//            'postalcode' => 'required|string|max:10',
+//            'housenumber' => 'required|numeric|min:1',
+//            'streetname' => 'required|string|max:255',
+//            'city' => 'required|string|max:255',
+//            'hours' => 'required|numeric|min:0',
+//            'contract_type' => 'required|string',
+//            'description' => 'nullable|string',
+////            'requirement' => 'nullable|string',
+//            'requirements' => 'array', // Validatie dat het een array is
+//                'requirements.*' => 'string|nullable', // Elk item moet een string zijn of null
+//            'image_url' => 'nullable|image|max:2048',
+//            'waiting' => 'nullable|integer|min:0', // Optioneel veld
+//            'available_positions' => 'nullable|integer|min:0', // Optioneel veld
+//            'employer_id' => 'nullable|integer|min:0', // Laat employer_id optioneel
+//        ]
+////            , [
+////            // Name Validatie
+////            'name.required' => 'Een naam is verplicht.',
+////            'name.string' => 'De naam moet een tekst zijn.',
+////            'name.max' => 'De naam mag niet meer dan 255 tekens bevatten.',
+////
+////            // Salary Validatie
+////            'salary.required' => 'Een salarisindicatie is verplicht.',
+////            'salary.numeric' => 'Het salaris moet een numerieke waarde zijn.',
+////            'salary.min' => 'Het salaris mag niet negatief zijn.',
+////
+////            // Postcode Validatie
+////            'postalcode.required' => 'Een postcode is verplicht.',
+////            'postalcode.string' => 'De postcode moet een tekst zijn.',
+////            'postalcode.max' => 'De postcode mag niet meer dan 10 tekens bevatten.',
+////
+////            // Huisnummer Validatie
+////            'housenumber.required' => 'Een huisnummer is verplicht.',
+////            'housenumber.numeric' => 'Het huisnummer moet een numerieke waarde zijn.',
+////            'housenumber.min' => 'Het huisnummer moet minstens 1 zijn.',
+////
+////            // Straatnaam Validatie
+////            'streetname.required' => 'Een straatnaam is verplicht.',
+////            'streetname.string' => 'De straatnaam moet een tekst zijn.',
+////            'streetname.max' => 'De straatnaam mag niet meer dan 255 tekens bevatten.',
+////
+////            // Plaats Validatie
+////            'city.required' => 'Een plaats is verplicht.',
+////            'city.string' => 'De plaats moet een tekst zijn.',
+////            'city.max' => 'De plaats mag niet meer dan 255 tekens bevatten.',
+////
+////            // Uren Validatie
+////            'hours.required' => 'Het aantal uren per week is verplicht.',
+////            'hours.numeric' => 'Het aantal uren moet een numerieke waarde zijn.',
+////            'hours.min' => 'Het aantal uren mag niet kleiner zijn dan 0.',
+////
+////            // Contract Type Validatie
+////            'contract_type.required' => 'Het type contract is verplicht.',
+////            'contract_type.string' => 'Het type contract moet een tekst zijn.',
+////
+////            // Beschrijving Validatie
+////            'description.string' => 'De beschrijving moet een tekst zijn.',
+////
+////            // Vereisten Validatie
+////            'requirement.string' => 'De vereisten moeten een tekst zijn.',
+////
+////            // Afbeelding Validatie
+////            'image_url.image' => 'Het geüploade bestand moet een afbeelding zijn.',
+////            'image_url.max' => 'De afbeelding mag niet groter zijn dan 2 MB.',
+////
+////            // Wachtende Validatie
+////            'waiting.integer' => 'Het aantal wachtende moet een geheel getal zijn.',
+////            'waiting.min' => 'Het aantal wachtende mag niet kleiner zijn dan 0.',
+////
+////            // Beschikbare Posities Validatie
+////            'available_positions.integer' => 'Het aantal beschikbare posities moet een geheel getal zijn.',
+////            'available_positions.min' => 'Het aantal beschikbare posities mag niet kleiner zijn dan 0.',
+////
+////            // Werkgever-ID Validatie
+////            'employer_id.integer' => 'De werkgever-ID moet een geheel getal zijn.',
+////            'employer_id.min' => 'De werkgever-ID mag niet kleiner zijn dan 0.',
+////        ]
+//);
+//
+//
+//
+//
+//        $vacancy = new Vacancy();
+//        $employerId = auth()->user()->employer_id;
+//
+////        insert into "vacancies" ("name", "salary", "location", "hours", "contract_type", "description", "requirement", "waiting", "available_positions", "employer_id", "updated_at", "created_at")
+////        values (Dawn Peck, Sed ipsum sequi sed, Sint deleniti fugit, 17, full-time, Porro voluptatum fug, ?, 1, 1, 1, 2024-12-04 12:44:21, 2024-12-04 12:44:21)
+//
+//        // Afbeelding uploaden
+//        if ($request->hasFile('image_url')) {
+//            $nameOfFile = $request->file('image_url')->store('images', 'public'); // Geen slash aan het begin
+//            $vacancy->image_url = $nameOfFile;
+//        }
+//
+//
+//        $vacancy->name = $request->input('name');
+//        $vacancy->salary = $request->input('salary');
+////        $vacancy->location = $request->input('location');
+//        $vacancy->hours = $request->input('hours');
+//        $vacancy->contract_type = $request->input('contract_type');
+//        $vacancy->description = $request->input('description');
+//        $vacancy->requirement = $request->input('requirement');
+//        $vacancy->postalcode = $request->input('postalcode');
+//        $vacancy->housenumber = $request->input('housenumber');
+//        $vacancy->streetname = $request->input('streetname');
+//        $vacancy->city = $request->input('city');
+//
+//// Check of de velden zijn ingevuld of stel een standaardwaarde in
+//        $vacancy->waiting = $request->input('waiting', 3); // Standaardwaarde 3
+//        $vacancy->available_positions = $request->input('available_positions', 1); // Standaardwaarde null
+//        $vacancy->employer_id = $employerId;
+//
+////        dd($vacancy);
+//        $vacancy->save();
+//
+//
+//        return redirect()->route('mijn-vacatures.index');
+//
+//    }
+
     public function store(Request $request)
     {
-        //
-
         $request->validate([
             'name' => 'required|string|max:255',
             'salary' => 'required|numeric|min:0',
@@ -43,82 +168,14 @@ class VacancyController extends Controller
             'hours' => 'required|numeric|min:0',
             'contract_type' => 'required|string',
             'description' => 'nullable|string',
-            'requirement' => 'nullable|string',
+            'requirements' => 'array', // Valideer als array
+            'requirements.*' => 'string|nullable', // Elk item in de array
             'image_url' => 'nullable|image|max:2048',
-            'waiting' => 'nullable|integer|min:0', // Optioneel veld
-            'available_positions' => 'nullable|integer|min:0', // Optioneel veld
-            'employer_id' => 'nullable|integer|min:0', // Laat employer_id optioneel
-        ], [
-            // Name Validatie
-            'name.required' => 'Een naam is verplicht.',
-            'name.string' => 'De naam moet een tekst zijn.',
-            'name.max' => 'De naam mag niet meer dan 255 tekens bevatten.',
-
-            // Salary Validatie
-            'salary.required' => 'Een salarisindicatie is verplicht.',
-            'salary.numeric' => 'Het salaris moet een numerieke waarde zijn.',
-            'salary.min' => 'Het salaris mag niet negatief zijn.',
-
-            // Postcode Validatie
-            'postalcode.required' => 'Een postcode is verplicht.',
-            'postalcode.string' => 'De postcode moet een tekst zijn.',
-            'postalcode.max' => 'De postcode mag niet meer dan 10 tekens bevatten.',
-
-            // Huisnummer Validatie
-            'housenumber.required' => 'Een huisnummer is verplicht.',
-            'housenumber.numeric' => 'Het huisnummer moet een numerieke waarde zijn.',
-            'housenumber.min' => 'Het huisnummer moet minstens 1 zijn.',
-
-            // Straatnaam Validatie
-            'streetname.required' => 'Een straatnaam is verplicht.',
-            'streetname.string' => 'De straatnaam moet een tekst zijn.',
-            'streetname.max' => 'De straatnaam mag niet meer dan 255 tekens bevatten.',
-
-            // Plaats Validatie
-            'city.required' => 'Een plaats is verplicht.',
-            'city.string' => 'De plaats moet een tekst zijn.',
-            'city.max' => 'De plaats mag niet meer dan 255 tekens bevatten.',
-
-            // Uren Validatie
-            'hours.required' => 'Het aantal uren per week is verplicht.',
-            'hours.numeric' => 'Het aantal uren moet een numerieke waarde zijn.',
-            'hours.min' => 'Het aantal uren mag niet kleiner zijn dan 0.',
-
-            // Contract Type Validatie
-            'contract_type.required' => 'Het type contract is verplicht.',
-            'contract_type.string' => 'Het type contract moet een tekst zijn.',
-
-            // Beschrijving Validatie
-            'description.string' => 'De beschrijving moet een tekst zijn.',
-
-            // Vereisten Validatie
-            'requirement.string' => 'De vereisten moeten een tekst zijn.',
-
-            // Afbeelding Validatie
-            'image_url.image' => 'Het geüploade bestand moet een afbeelding zijn.',
-            'image_url.max' => 'De afbeelding mag niet groter zijn dan 2 MB.',
-
-            // Wachtende Validatie
-            'waiting.integer' => 'Het aantal wachtende moet een geheel getal zijn.',
-            'waiting.min' => 'Het aantal wachtende mag niet kleiner zijn dan 0.',
-
-            // Beschikbare Posities Validatie
-            'available_positions.integer' => 'Het aantal beschikbare posities moet een geheel getal zijn.',
-            'available_positions.min' => 'Het aantal beschikbare posities mag niet kleiner zijn dan 0.',
-
-            // Werkgever-ID Validatie
-            'employer_id.integer' => 'De werkgever-ID moet een geheel getal zijn.',
-            'employer_id.min' => 'De werkgever-ID mag niet kleiner zijn dan 0.',
         ]);
 
-
-
-
-        $vacancy = new Vacancy();
+                $vacancy = new Vacancy();
         $employerId = auth()->user()->employer_id;
 
-//        insert into "vacancies" ("name", "salary", "location", "hours", "contract_type", "description", "requirement", "waiting", "available_positions", "employer_id", "updated_at", "created_at")
-//        values (Dawn Peck, Sed ipsum sequi sed, Sint deleniti fugit, 17, full-time, Porro voluptatum fug, ?, 1, 1, 1, 2024-12-04 12:44:21, 2024-12-04 12:44:21)
 
         // Afbeelding uploaden
         if ($request->hasFile('image_url')) {
@@ -126,30 +183,29 @@ class VacancyController extends Controller
             $vacancy->image_url = $nameOfFile;
         }
 
-
+        $vacancy = new Vacancy();
         $vacancy->name = $request->input('name');
         $vacancy->salary = $request->input('salary');
-//        $vacancy->location = $request->input('location');
-        $vacancy->hours = $request->input('hours');
-        $vacancy->contract_type = $request->input('contract_type');
-        $vacancy->description = $request->input('description');
-        $vacancy->requirement = $request->input('requirement');
         $vacancy->postalcode = $request->input('postalcode');
         $vacancy->housenumber = $request->input('housenumber');
         $vacancy->streetname = $request->input('streetname');
         $vacancy->city = $request->input('city');
+        $vacancy->hours = $request->input('hours');
+        $vacancy->contract_type = $request->input('contract_type');
+        $vacancy->description = $request->input('description');
+        $vacancy->requirement = json_encode($request->input('requirements', [])); // JSON-string opslaan
 
-// Check of de velden zijn ingevuld of stel een standaardwaarde in
+
+        //// Check of de velden zijn ingevuld of stel een standaardwaarde in
         $vacancy->waiting = $request->input('waiting', 3); // Standaardwaarde 3
         $vacancy->available_positions = $request->input('available_positions', 1); // Standaardwaarde null
         $vacancy->employer_id = $employerId;
 
-//        dd($vacancy);
+
+
         $vacancy->save();
 
-
-        return redirect()->route('mijn-vacatures.index');
-
+        return redirect()->route('mijn-vacatures.index')->with('success', 'Vacature succesvol aangemaakt.');
     }
 
     /**
